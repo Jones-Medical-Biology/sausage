7f2f6b680cde4de7154b4021dac10335a0daa8bf
Noah Jones <noahtjones@gmail.com>
2023-11-12 11:03:04 -0500

    defined datatypes and added bytestring

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,55 @@
+{-# LANGUAGE DataKinds #-} -- TemplateHaskell, GADTs,
+
 module Lib
     ( someFunc
     , quicksort
     , umap
     , umap'
     ) where
+
+-- data GeneExpressionData
+--   = GeneCode String
+--   | RelativeExpression Float
+--   | SampleId String
+
+data GeneCode = GeneCode String
+  deriving (Show, Eq)
+
+data SampleId = SampleId String
+  deriving (Show, Eq)
+
+data RelativeExpression = Float
+  deriving (Show, Eq)
+
+
+data GeneExpression = GeneExpression
+    { genecode :: GeneCode
+    , relative_expression :: RelativeExpression }
+  deriving (Show, Eq)
+
+data GeneExpressionSample = GeneExpressionSample
+    { sample_id :: SampleId
+    , expression_set :: [GeneExpression] }
+  deriving (Show, Eq)
+
+data DnaBase = Adenine | Thymine | Cytosine | Guanine
+  deriving (Show, Eq, Enum)
+
+data FastaSequence = FastaSequence
+  { sequence_header :: (Maybe GeneCode, Maybe SampleId)
+  , sequence_data :: [DnaBase] }
+  deriving (Show)
+
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a) }
+
+-- parse_fasta_head :: String -> Parser String
+-- parse_fasta_head x = Parser f
+--   where
+--     f (y:ys) 
+--       | y == x = Just (y, ys)
+--       | otherwise = Nothing
+--     f [] = Nothing
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
