3147d8b668cd29d7bdfda8438d23783fd3fc9719
Noah Jones <noahtjones@gmail.com>
2024-06-30 22:34:55 -0400

    added tutorial code

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,8 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do c <- getContents
+          case parse csvFile "(stdin)" c of
+               Left e -> do putStrLn "Error parsing input:"
+                            print e
+               Right r -> mapM_ print r
